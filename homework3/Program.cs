776756dc423ed8ee7fefb8ee0f8668882146940b
STAN-65031 <lox.ibany@gmail.com>
2022-08-05 23:46:37 +0300

    3 задание

---
+++ homework3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int evenNumber = number1 % 2;
+
+if (evenNumber == 0){
+     Console.WriteLine("число чётное");
+}
+else;
+
+    if (evenNumber != 0){
+        Console.WriteLine("число нечётное");
+    
+}
+
+
+    
